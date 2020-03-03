bc14c9139309fc700ee670ea909f3bb9e1191c99
phamhuuhoang99 <44650926+phamhuuhoang99@users.noreply.github.com>
2020-03-03 22:33:07 +0700

    Update Program.cs

--- Test_Mot_Chut/Program.cs
+++ Test_Mot_Chut/Program.cs
@@ -14,6 +14,7 @@ namespace Test_Mot_Chut
         [STAThread]
         static void Main()
         {
+            ;;;;;;;;;;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormBegin());
